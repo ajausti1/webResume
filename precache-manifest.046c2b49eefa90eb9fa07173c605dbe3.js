95df3d81419547f71db748fc523217a74993bb74
Andrew Austin <andrew@dataportfol.io>
2019-03-05 21:34:34 +0000

    Updates

---
+++ precache-manifest.046c2b49eefa90eb9fa07173c605dbe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d80b7939e17b66ab03c1",
+    "url": "/webResume/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "d80b7939e17b66ab03c1",
+    "url": "/webResume/static/js/main.d80b7939.chunk.js"
+  },
+  {
+    "revision": "f64090996c3ccbb7e7bc",
+    "url": "/webResume/static/js/runtime~main.f6409099.js"
+  },
+  {
+    "revision": "4b95350b2236d8ff47ed",
+    "url": "/webResume/static/js/2.4b95350b.chunk.js"
+  },
+  {
+    "revision": "834152b686b4925361a51b71ad3bc31e",
+    "url": "/webResume/index.html"
+  }
+];
